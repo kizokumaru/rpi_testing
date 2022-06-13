3bc8b06495d9de81bf10f7e180c7e4a0b7ff4bd1
kizokumaru <nestorlopezroyo@gmail.com>
2022-06-13 20:49:08 +0200

    corregimos test route

--- lanup/backend/webserver/test/routes.test.js
+++ lanup/backend/webserver/test/routes.test.js
@@ -2,36 +2,48 @@ import fs from 'node:fs';
 import Mapperlan from '../src/utils/mapperlan.mjs';
 //const jsondata = JSON.parse(fs.readFileSync('../database/nmap_results.json', 'utf-8'));
 const regex = /^MAC Address: /gm;
+const regexIp = /^Nmap scan report for /gm;
 const subst = '';
 const sentence = 'nmap --privileged -sn 192.168.0.0/24';
-async function test(){
-    
-    let result = await new Mapperlan().execute(sentence).then(
-        (data) => {
-          let jsonData =  JSON.parse(fs.readFileSync('../database/nmap_results.json', 'utf-8'));
-          let lines = data.split('\n');
-          let res =[];
-          lines.shift();
-          lines.shift();
-          lines.pop();
-          lines.pop();
-          lines.forEach((currentValue, index, arr) => {
-            if (currentValue.match('^MAC Address:')) {
-                let mac = currentValue.replace(regex, subst).substring(0, 17)
-                console.log('MAC: ' + mac)
-                console.log('Objecto: ' + jsonData.data.hosts[mac])
-                jsonData.data.hosts[mac].host = "Corriendo";
-                if(jsonData.data.hosts[mac]==undefined){
-                  new Error('Mac ' + mac + ' no esta controlada, incorporarla en fichero json.')
-                }
-                console.log('Objeto Mod: ' + jsonData.data.hosts[mac]);
-              }
-          })
-          return JSON.stringify(jsonData);
-        }
-      );
+async function test() {
 
-    return result;
+  let result = await new Mapperlan().execute(sentence).then(
+    (data) => {
+      let jsonData = JSON.parse(fs.readFileSync('../database/nmap_results.json', 'utf-8'));
+      let lines = data.split('\n');
+      let res = [];
+      lines.shift();
+      lines.shift();
+      lines.pop();
+      lines.pop();
+      console.log(lines.length);
+      lines.forEach((currentValue, index, arr) => {
+
+        console.log(currentValue);
+
+        if (currentValue.match('^MAC Address:')) {
+          let mac = currentValue.replace(regex, subst).substring(0, 17);
+          let ip = arr[index + 1].replace(regexIp, subst);
+          console.log(ip);
+          console.log('MAC: ' + mac + ' index: ' + index)
+          console.log('Objeto: ' + JSON.stringify(jsonData.data.hosts[mac]))
+
+          if (jsonData.data.hosts[mac] == undefined) {
+            new Error('Mac ' + mac + ' no esta controlada, incorporarla en fichero json.')
+          } else {
+            jsonData.data.hosts[mac].host = "Live";
+            jsonData.data.hosts[mac].nmap = ip;
+            console.log('Objeto Mod: ' + JSON.stringify(jsonData.data.hosts[mac]));
+          }
+        }
+
+      })
+      return JSON.stringify(jsonData);
+    }
+  );
+
+  return result;
 }
 
-let value = test().then((data)=> console.log(data));
+let value = test()
+  .then((data) => console.log(data));
